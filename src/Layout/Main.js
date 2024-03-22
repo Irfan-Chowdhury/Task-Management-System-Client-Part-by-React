9ab6c9ec03b1f0e61145b0347754cfd642ea1a59
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2024-03-22 22:25:51 +0600

    Initial Setup Done

---
+++ src/Layout/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Main = () => {
+    return (
+        <div>
+            <Container>
+                <h1>Welcome To React</h1>
+            </Container>
+        </div>
+    );
+};
+
+export default Main;
